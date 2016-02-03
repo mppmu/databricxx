791ab08112aa50e20e1d82e43c9d1c85bef35100
Oliver Schulz <oschulz@mpp.mpg.de>
2016-02-03 15:10:43 +0100

    Added TextFilePrinter<T> as generalization of TextFileWriter

--- src/textbrics.h
+++ src/textbrics.h
@@ -45,14 +45,14 @@ public:
 
 
 
-class TextFileWriter: public ReducerBric {
+template<typename T> class TextFilePrinter: public ReducerBric {
 protected:
 	ManagedOutputStream m_outputStream;
 
 public:
-	Input<std::string> input{this, "", "Input line"};
+	Input<T> input{this, "", "Input value"};
 
-	Param<std::string> target{this, "target", "Output filename", "stdout"};
+	Param<std::string> target{this, "target", "Output filename", "-"};
 
 	Output<ssize_t> output{this, "", "Number of lines in output"};
 
@@ -64,6 +64,32 @@ public:
 
 	using ReducerBric::ReducerBric;
 };
+
+
+template<typename T> void TextFilePrinter<T>::newReduction() {
+	dbrx_log_trace("TextFilePrinter \"%s\", opening output \"%s\""_format(absolutePath(), target.get()));
+	try {
+		m_outputStream.open(target);
+	} catch (std::runtime_error &e) {
+		throw runtime_error("Can't open \"%s\" for output in bric \"%s\": %s"_format(target.get(), absolutePath(), e.what()));
+	}
+}
+
+
+template<typename T> void TextFilePrinter<T>::processInput() {
+	using namespace std;
+	m_outputStream.stream() << input.get() << '\n' << flush;
+	if (! m_outputStream.stream()) throw runtime_error("Output to \"%s\" failed in bric \"%s\""_format(target.get(), absolutePath()));
+}
+
+
+template<typename T> void TextFilePrinter<T>::finalizeReduction() {
+	dbrx_log_trace("TextFilePrinter \"%s\", closing output \"%s\""_format(absolutePath(), target.get()));
+	m_outputStream.close();
+}
+
+
+using TextFileWriter = TextFilePrinter<std::string>;
 
 
 } // namespace dbrx
